b827b3aaf6f267ea62c4ae361a34c92adbed03e2
secreal <hendryhendratno@gmail.com>
2026-04-24 00:00:12 +0700

    feat(theme): tune checklist status colors

--- src/app/GitExtUtils/GitUI/Theming/OtherColors.cs
+++ src/app/GitExtUtils/GitUI/Theming/OtherColors.cs
@@ -7,7 +7,7 @@ public static class OtherColors
     public static readonly Color MergeConflictsColor = Color.FromArgb(230, 99, 99).AdaptBackColor();
     public static readonly Color PanelBorderColor = Color.FromArgb(224, 224, 224).AdaptBackColor();
     public static readonly Color PanelMessageWarningColor = Color.FromArgb(230, 99, 99).AdaptBackColor();
-    public static readonly Color BrightGreen = Color.FromArgb(128, 255, 128);
-    public static readonly Color BrightYellow = Color.FromArgb(255, 255, 128);
-    public static readonly Color BrightRed = Color.FromArgb(255, 128, 128);
+    public static readonly Color BrightGreen = Color.FromArgb(15, 157, 88);
+    public static readonly Color BrightYellow = Color.FromArgb(253, 216, 53);
+    public static readonly Color BrightRed = Color.FromArgb(219, 68, 55);
 }
